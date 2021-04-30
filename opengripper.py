a7ab78325da4da994f61083e835c17b91608c563
Locobot <weichenglau1991@gmail.com>
2021-04-30 06:40:38 -0500

    moveto now working

--- opengripper.py
+++ opengripper.py
@@ -4,6 +4,9 @@ type 'open' or 'close' when prompted.
 '''
 
 import json
+import tkinter as tk
+import numpy as np
+
 from pyrobot import Robot
 
 robot = Robot('locobot')
@@ -21,39 +24,20 @@ def move(position):
     robot.arm.set_ee_pose_pitch_roll(position, pitch=1.57, roll=0, plan=True)
 
 def shift_left():
-    robot.arm.move_ee_xyz(np.array([-0.5,0,0]))
+    # TODO: this is not working now
+    robot.arm.move_ee_xyz(np.array((0,0.05,0)))
 
 # Entry to move arm
+window = tk.Tk()
+
 pos_var = tk.StringVar(value='0.3,0,0.3')
 pos_entry = tk.Entry(text='Desired pos',textvariable=pos_var)
 
 def moveto():
     pos = f'[{pos_var.get()}]'
     robot.arm.set_ee_pose_pitch_roll(json.loads(pos), pitch=1.57, roll=0, plan=True)
-
-# while True:
-#     command = input("Command (open, close, move, exit): ")
-#     if command.lower() == 'open':
-#         open_gripper()
-#     elif command.lower() == 'close':
-#         close_gripper()
-#     elif command.lower() == 'move':
-#         # move to absolute position
-#         position = input('Enter a list [x,y,z]: ')
-#         # change string to python list
-#         position = json.loads(position)
-#         move(position)
-#     elif command.lower() == 'exit':
-#         robot.arm.set_ee_pose_pitch_roll([0.4, 0., 0.3], pitch=1.57, roll=0, plan=True)
-#         break
-#     else:
-#         print('please enter a valid command.')
-
-
-# TODO: add GUI for convenience? 
-import tkinter as tk
-
-window = tk.Tk()
+    # xyz_var.set(str(robot.arm.pose_ee[0]))
+    print(robot.arm.pose_ee[0])
 
 open_button = tk.Button(
     text="Open",
@@ -85,7 +69,7 @@ left_button = tk.Button(
 )
 
 move_button = tk.Button(
-    text="Left",
+    text="MoveTo",
     width=25,
     height=5,
     bg="black",
@@ -94,9 +78,10 @@ move_button = tk.Button(
 )
 
 
-
-current_xyz = robot.arm.pose_ee[0]
-xyz_label = tk.Label(text=f"{current_xyz}")
+xyz_var = tk.StringVar()
+xyz_var = str(robot.arm.pose_ee[0])
+# current_xyz = robot.arm.pose_ee[0]
+xyz_label = tk.Label(textvariable=xyz_var)
 
 xyz_label.pack()
 open_button.pack()
@@ -106,3 +91,22 @@ pos_entry.pack()
 move_button.pack()
 
 window.mainloop()
+
+
+# while True:
+#     command = input("Command (open, close, move, exit): ")
+#     if command.lower() == 'open':
+#         open_gripper()
+#     elif command.lower() == 'close':
+#         close_gripper()
+#     elif command.lower() == 'move':
+#         # move to absolute position
+#         position = input('Enter a list [x,y,z]: ')
+#         # change string to python list
+#         position = json.loads(position)
+#         move(position)
+#     elif command.lower() == 'exit':
+#         robot.arm.set_ee_pose_pitch_roll([0.4, 0., 0.3], pitch=1.57, roll=0, plan=True)
+#         break
+#     else:
+#         print('please enter a valid command.')
