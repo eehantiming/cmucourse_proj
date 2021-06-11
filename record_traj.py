49e941eb81e533e033a7ea8d680840679b185d6c
Locobot <weichenglau1991@gmail.com>
2021-06-11 01:16:35 -0500

    updated with savgol filter

--- record_traj.py
+++ record_traj.py
@@ -7,12 +7,13 @@ load_pyrobot_env && python3 record_traj.py
 
 import time
 import numpy as np
+from scipy.signal import savgol_filter
 
 from pyrobot import Robot
 
 # Time in seconds to record for
-DURATION = 3
-TIME_BETWEEN_STEPS = 0.1
+DURATION = 15
+TIME_BETWEEN_STEPS = 0.01
 
 arm_config = dict(control_mode='torque')
 robot = Robot('locobot', arm_config=arm_config)
@@ -39,11 +40,11 @@ print("\nStarting to record")
 
 # Save first position and velocity
 old_pos = np.concatenate((robot.arm.pose_ee[0]), axis=None)
-old_vel = np.zeros_like(old_pos)
-acc = np.zeros_like(old_pos)
+# old_vel = np.zeros_like(old_pos)
+# acc = np.zeros_like(old_pos)
 pos_array.append(old_pos)
-vel_array.append(old_vel)
-acc_array.append(acc)
+# vel_array.append(old_vel)
+# acc_array.append(acc)
 
 start = time.time()
 while elaptime < DURATION:
@@ -51,15 +52,23 @@ while elaptime < DURATION:
     # TODO: Check data format
     pos = np.concatenate((robot.arm.pose_ee[0]), axis=None)
     # use previous values to estimate differential
-    vel = (pos - old_pos) / TIME_BETWEEN_STEPS
-    acc = (vel - old_vel) / TIME_BETWEEN_STEPS
+    # vel = (pos - old_pos) / TIME_BETWEEN_STEPS
+    # acc = (vel - old_vel) / TIME_BETWEEN_STEPS
     time_array.append(elaptime)
     pos_array.append(pos)
-    vel_array.append(vel)
-    acc_array.append(acc)
-    old_vel = vel
-    old_pos = pos
+    # vel_array.append(vel)
+    # acc_array.append(acc)
+    # old_vel = vel
+    # old_pos = pos
     time.sleep(TIME_BETWEEN_STEPS)
+
+pos_array = np.array(pos_array)
+vel_array = (pos_array[1:, :] - pos_array[:-1, :]) / TIME_BETWEEN_STEPS
+for i in range(vel_array.shape[1]): 
+	vel_array[:, i] = savgol_filter(vel_array[:, i], 21, 5)
+vel_array = np.vstack([vel_array, vel_array[-1:, :]]) 
+acc_array = (vel_array[1:, :] - vel_array[:-1, :]) / TIME_BETWEEN_STEPS
+acc_array = np.vstack([acc_array, acc_array[-1:, :]])
 
 # Save lists. auto saves to .npz 
 filename = './recordings/' + input('Save as: ')
@@ -67,3 +76,9 @@ np.savez(filename, time=time_array, pos=pos_array, vel=vel_array, acc=acc_array)
 print(f'Saved to {filename}.npz!')
 
 # TODO: add plots for pos, vel and acc
+# vel = (x[1:, :] - x[:-1, :]) / dt
+# for i in range(vel.shape[1]): 
+# 	vel[:, i] = savgol_filter(vel[:, i], 21, 5)
+# 	vel = np.vstack([vel, vel[-1:, :]]) 
+# 	acc = (vel[1:, :] - vel[:-1, :]) / dt
+# 	acc = np.vstack([acc, acc[-1:, :]])
--- replay_traj.py
+++ replay_traj.py
@@ -2,40 +2,39 @@
 Replays the recorded trajectory by end effector position.
 
 roslaunch locobot_control main.launch use_arm:=true torque_control:=false use_rviz:=false
-python3 replay_traj.py
+python3 replay_traj.py forward
 '''
 import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 from pyrobot import Robot
 
-# robot = Robot('locobot')
-config = dict(moveit_planner='RRTstarkConfigDefault')
-robot = Robot('locobot', arm_config=config)
+from DMP import plot_trajectory
+
+robot = Robot('locobot')
 # Change file to load here.
-# filename = 'curve.npz'
 filename = f'./recordings/{sys.argv[1]}.npz'
 print(f'Loading from {filename}')
 
 positions = np.load(filename)['pos']
-velocities = np.load(filename)['vel']
-accelerations = np.load(filename)['acc']
+# velocities = np.load(filename)['vel']
+# accelerations = np.load(filename)['acc']
 print(f'Number of points: {len(positions)}')
-# print(f'lowest:{min(positions)}')
 
-# Show plot 
-fig, axes = plt.subplots(3)
-axes[0].plot([positions[i][0] for i in range(len(positions))])
-axes[1].plot([positions[i][1] for i in range(len(positions))])
-axes[2].plot([positions[i][2] for i in range(len(positions))])
-plt.show()
+# # Show xyz plot 
+# fig, axes = plt.subplots(3)
+# axes[0].plot([positions[i][0] for i in range(len(positions))])
+# axes[1].plot([positions[i][1] for i in range(len(positions))])
+# axes[2].plot([positions[i][2] for i in range(len(positions))])
+# plt.show()
+plot_trajectory(positions)
 
 # Move arm
 for position in positions:
     position = position.tolist()
     robot.arm.set_ee_pose_pitch_roll(position, pitch=1.57, roll=0, plan=False)
+
 
 
 
